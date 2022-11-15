0882b515af40caf6503dff3c742dec93eaf8123c
Liam Callaghan <liam.callaghan2@my.bcit.ca>
2022-11-15 10:41:54 -0800

    repopulate shopping list

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
-let greet = userName => console.log('Hello  ${userName}');
+//function greet(userName, time = "Day") {
+  //  console.log('Good ${time} ${userName}');
+//}
 
-greet("Neda");
-greet("Tin");
+//greet("Neda", "Night");
+
+const unorderedListElement = document.querySelector(".shopping")
+function populateList (list)
+{
+    for (let i = 0 ; i <list.length ; i++)
+    {
+        console.log(list[i])
+        const listItemElement = document.createElement("li");
+        listItemElement.textContent = list[i];
+        unorderedListElement.appendChild(listItemElement);
+    }
+}
+
+let shoppingList = ["cheese", "bread", "green pepper"];
+
+populateList(shoppingList)
